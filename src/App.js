61069f036c4128b3a973f3858c1b960902203632
miyazakimaiko <myzkmik19922@gmail.com>
2023-01-14 01:24:46 +0000

    Migrated from coffeastock-client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Terms from './pages/terms';
+import Privacy from './pages/privacy';
+import ScrollBackButton from './elements/ScrollBackButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-sans text-xs md:text-sm text-burnt-sienna">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          {/* <Route exact path="500" element={<ServerError />} />   */}
+          <Route exact path="terms" element={<Terms />} />
+          <Route exact path="privacy" element={<Privacy />} />
+          {/* <Route exact path='/login' element={authenticated ? <Navigate to="/app/dashboard" />  : login} /> */}
+          {/* <Route exact path='/register' element={<Register />} /> */}
+        </Routes>
+
+        <ScrollBackButton />
+      </Router>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,72 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['Kumbh Sans', 'system-ui', 'sans-serif'],
+      'serif': ['ui-serif', 'Georgia'],
+    },
+    extend: {
+      colors: {
+        'burnt-sienna': '#484040',
+        'burnt-sienna-darker': '#372d27',
+        'creme': '#F4F3EE',
+        'lightgrey': '#E6E6E6',
+        'greycreme': '#e9e8e3',
+        'green': '#93CCAC',
+        'deep-green': '#1da359',
+        'deep-blue': '#24abcf',
+        'blue': '#68B3C7',
+        'ash-blue': '#a8c8c8',
+        'orange': '#EA9470',
+        'deep-orange': '#ee6931',
+        'lime': '#D6E4A3',
+        'yellow': '#F3BB44',
+        'red': '#EB5E28',
+        'pink-red': '#C65673',
+        'pink': '#e19193',
+        'ash-pink': '#E8C1B4',
+        'purple': '#9a92c4',
+      },
+      width: {
+        '800px': '800px'
+      },
+      height: {
+        'fit': 'fit-content'
+      },
+      maxWidth: {
+        '1/2': '50%',
+        '10': '10rem',
+        '60pc': '60%',
+        '300px': '300px',
+        '980px': '980px',
+      },
+      minWidth: {
+        '21': '21rem',
+        '250px': '250px',
+        '150px': '150px'
+      },
+      maxHeight: {
+        '600px': '600px'
+      },
+      transitionProperty: {
+        'width': 'width'
+      }
+    },
+  },
+  variants: {
+    extend: {
+      ringWidth: ['focus-visible'],
+      ringColor: ['focus-visible'],
+      ringOffsetWidth: ['focus-visible'],
+      ringOffsetColor: ['focus-visible'],
+      backgroundColor: ['odd', 'even'],
+    },
+  },
+  plugins: [],
+}
